0ce6dddea59c020d5dc0d2ba056c58b4c78ddf94
matthewlevy67 <44776874+matthewlevy67@users.noreply.github.com>
2018-11-28 21:46:22 -0500

    Add files via upload

---
+++ Gradecalculator/Gradecalculator/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Gradecalculator
+{
+    partial class averagecal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblClassOne = new System.Windows.Forms.Label();
+            this.lblClassTwo = new System.Windows.Forms.Label();
+            this.lblClassThree = new System.Windows.Forms.Label();
+            this.lblClassFour = new System.Windows.Forms.Label();
+            this.lblClassFive = new System.Windows.Forms.Label();
+            this.lblClassSix = new System.Windows.Forms.Label();
+            this.txtClassOne = new System.Windows.Forms.TextBox();
+            this.txtClassTwo = new System.Windows.Forms.TextBox();
+            this.txtClassThree = new System.Windows.Forms.TextBox();
+            this.txtClassFour = new System.Windows.Forms.TextBox();
+            this.txtClassFive = new System.Windows.Forms.TextBox();
+            this.txtClassSix = new System.Windows.Forms.TextBox();
+            this.lblLetterOne = new System.Windows.Forms.Label();
+            this.lblLetterTwo = new System.Windows.Forms.Label();
+            this.lblLetterThree = new System.Windows.Forms.Label();
+            this.lblLetterFour = new System.Windows.Forms.Label();
+            this.lblLabelFive = new System.Windows.Forms.Label();
+            this.lblLetterSix = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(587, 919);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(292, 77);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblClassOne
+            // 
+            this.lblClassOne.AutoSize = true;
+            this.lblClassOne.Location = new System.Drawing.Point(265, 170);
+            this.lblClassOne.Name = "lblClassOne";
+            this.lblClassOne.Size = new System.Drawing.Size(132, 32);
+            this.lblClassOne.TabIndex = 1;
+            this.lblClassOne.Text = "Class #1 ";
+            // 
+            // lblClassTwo
+            // 
+            this.lblClassTwo.AutoSize = true;
+            this.lblClassTwo.Location = new System.Drawing.Point(265, 286);
+            this.lblClassTwo.Name = "lblClassTwo";
+            this.lblClassTwo.Size = new System.Drawing.Size(125, 32);
+            this.lblClassTwo.TabIndex = 2;
+            this.lblClassTwo.Text = "Class #2";
+            // 
+            // lblClassThree
+            // 
+            this.lblClassThree.AutoSize = true;
+            this.lblClassThree.Location = new System.Drawing.Point(265, 396);
+            this.lblClassThree.Name = "lblClassThree";
+            this.lblClassThree.Size = new System.Drawing.Size(125, 32);
+            this.lblClassThree.TabIndex = 3;
+            this.lblClassThree.Text = "Class #3";
+            // 
+            // lblClassFour
+            // 
+            this.lblClassFour.AutoSize = true;
+            this.lblClassFour.Location = new System.Drawing.Point(265, 501);
+            this.lblClassFour.Name = "lblClassFour";
+            this.lblClassFour.Size = new System.Drawing.Size(125, 32);
+            this.lblClassFour.TabIndex = 4;
+            this.lblClassFour.Text = "Class #4";
+            // 
+            // lblClassFive
+            // 
+            this.lblClassFive.AutoSize = true;
+            this.lblClassFive.Location = new System.Drawing.Point(265, 605);
+            this.lblClassFive.Name = "lblClassFive";
+            this.lblClassFive.Size = new System.Drawing.Size(125, 32);
+            this.lblClassFive.TabIndex = 5;
+            this.lblClassFive.Text = "Class #5";
+            // 
+            // lblClassSix
+            // 
+            this.lblClassSix.AutoSize = true;
+            this.lblClassSix.Location = new System.Drawing.Point(265, 703);
+            this.lblClassSix.Name = "lblClassSix";
+            this.lblClassSix.Size = new System.Drawing.Size(125, 32);
+            this.lblClassSix.TabIndex = 6;
+            this.lblClassSix.Text = "Class #6";
+            this.lblClassSix.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtClassOne
+            // 
+            this.txtClassOne.Location = new System.Drawing.Point(527, 167);
+            this.txtClassOne.Name = "txtClassOne";
+            this.txtClassOne.Size = new System.Drawing.Size(100, 38);
+            this.txtClassOne.TabIndex = 8;
+            // 
+            // txtClassTwo
+            // 
+            this.txtClassTwo.Location = new System.Drawing.Point(527, 280);
+            this.txtClassTwo.Name = "txtClassTwo";
+            this.txtClassTwo.Size = new System.Drawing.Size(100, 38);
+            this.txtClassTwo.TabIndex = 9;
+            // 
+            // txtClassThree
+            // 
+            this.txtClassThree.Location = new System.Drawing.Point(527, 390);
+            this.txtClassThree.Name = "txtClassThree";
+            this.txtClassThree.Size = new System.Drawing.Size(100, 38);
+            this.txtClassThree.TabIndex = 10;
+            // 
+            // txtClassFour
+            // 
+            this.txtClassFour.Location = new System.Drawing.Point(527, 495);
+            this.txtClassFour.Name = "txtClassFour";
+            this.txtClassFour.Size = new System.Drawing.Size(100, 38);
+            this.txtClassFour.TabIndex = 11;
+            // 
+            // txtClassFive
+            // 
+            this.txtClassFive.Location = new System.Drawing.Point(527, 599);
+            this.txtClassFive.Name = "txtClassFive";
+            this.txtClassFive.Size = new System.Drawing.Size(100, 38);
+            this.txtClassFive.TabIndex = 12;
+            // 
+            // txtClassSix
+            // 
+            this.txtClassSix.Location = new System.Drawing.Point(527, 700);
+            this.txtClassSix.Name = "txtClassSix";
+            this.txtClassSix.Size = new System.Drawing.Size(100, 38);
+            this.txtClassSix.TabIndex = 13;
+            // 
+            // lblLetterOne
+            // 
+            this.lblLetterOne.AutoSize = true;
+            this.lblLetterOne.Location = new System.Drawing.Point(743, 170);
+            this.lblLetterOne.Name = "lblLetterOne";
+            this.lblLetterOne.Size = new System.Drawing.Size(172, 32);
+            this.lblLetterOne.TabIndex = 15;
+            this.lblLetterOne.Tag = "";
+            this.lblLetterOne.Text = "lblLetterOne";
+            // 
+            // lblLetterTwo
+            // 
+            this.lblLetterTwo.AutoSize = true;
+            this.lblLetterTwo.Location = new System.Drawing.Point(743, 283);
+            this.lblLetterTwo.Name = "lblLetterTwo";
+            this.lblLetterTwo.Size = new System.Drawing.Size(171, 32);
+            this.lblLetterTwo.TabIndex = 16;
+            this.lblLetterTwo.Tag = "";
+            this.lblLetterTwo.Text = "lblLetterTwo";
+            // 
+            // lblLetterThree
+            // 
+            this.lblLetterThree.AutoSize = true;
+            this.lblLetterThree.Location = new System.Drawing.Point(743, 390);
+            this.lblLetterThree.Name = "lblLetterThree";
+            this.lblLetterThree.Size = new System.Drawing.Size(190, 32);
+            this.lblLetterThree.TabIndex = 17;
+            this.lblLetterThree.Tag = "";
+            this.lblLetterThree.Text = "lblLabelThree";
+            this.lblLetterThree.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // lblLetterFour
+            // 
+            this.lblLetterFour.AutoSize = true;
+            this.lblLetterFour.Location = new System.Drawing.Point(740, 495);
+            this.lblLetterFour.Name = "lblLetterFour";
+            this.lblLetterFour.Size = new System.Drawing.Size(174, 32);
+            this.lblLetterFour.TabIndex = 18;
+            this.lblLetterFour.Tag = "";
+            this.lblLetterFour.Text = "lblLabelFour";
+            // 
+            // lblLabelFive
+            // 
+            this.lblLabelFive.AutoSize = true;
+            this.lblLabelFive.Location = new System.Drawing.Point(747, 599);
+            this.lblLabelFive.Name = "lblLabelFive";
+            this.lblLabelFive.Size = new System.Drawing.Size(170, 32);
+            this.lblLabelFive.TabIndex = 19;
+            this.lblLabelFive.Tag = "";
+            this.lblLabelFive.Text = "lblLabelFive";
+            // 
+            // lblLetterSix
+            // 
+            this.lblLetterSix.AutoSize = true;
+            this.lblLetterSix.Location = new System.Drawing.Point(740, 700);
+            this.lblLetterSix.Name = "lblLetterSix";
+            this.lblLetterSix.Size = new System.Drawing.Size(158, 32);
+            this.lblLetterSix.TabIndex = 20;
+            this.lblLetterSix.Tag = "";
+            this.lblLetterSix.Text = "lblLetterSix";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 1015);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 32);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Here is Your Average";
+            this.label1.Click += new System.EventHandler(this.label1_Click_3);
+            // 
+            // lblCal
+            // 
+            this.lblCal.AutoSize = true;
+            this.lblCal.Location = new System.Drawing.Point(603, 1015);
+            this.lblCal.Name = "lblCal";
+            this.lblCal.Size = new System.Drawing.Size(0, 32);
+            this.lblCal.TabIndex = 24;
+            // 
+            // averagecal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1506, 1070);
+            this.Controls.Add(this.lblCal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblLetterSix);
+            this.Controls.Add(this.lblLabelFive);
+            this.Controls.Add(this.lblLetterFour);
+            this.Controls.Add(this.lblLetterThree);
+            this.Controls.Add(this.lblLetterTwo);
+            this.Controls.Add(this.lblLetterOne);
+            this.Controls.Add(this.txtClassSix);
+            this.Controls.Add(this.txtClassFive);
+            this.Controls.Add(this.txtClassFour);
+            this.Controls.Add(this.txtClassThree);
+            this.Controls.Add(this.txtClassTwo);
+            this.Controls.Add(this.txtClassOne);
+            this.Controls.Add(this.lblClassSix);
+            this.Controls.Add(this.lblClassFive);
+            this.Controls.Add(this.lblClassFour);
+            this.Controls.Add(this.lblClassThree);
+            this.Controls.Add(this.lblClassTwo);
+            this.Controls.Add(this.lblClassOne);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "averagecal";
+            this.Text = "Average Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblClassOne;
+        private System.Windows.Forms.Label lblClassTwo;
+        private System.Windows.Forms.Label lblClassThree;
+        private System.Windows.Forms.Label lblClassFour;
+        private System.Windows.Forms.Label lblClassFive;
+        private System.Windows.Forms.Label lblClassSix;
+        private System.Windows.Forms.TextBox txtClassOne;
+        private System.Windows.Forms.TextBox txtClassTwo;
+        private System.Windows.Forms.TextBox txtClassThree;
+        private System.Windows.Forms.TextBox txtClassFour;
+        private System.Windows.Forms.TextBox txtClassFive;
+        private System.Windows.Forms.TextBox txtClassSix;
+        private System.Windows.Forms.Label lblLetterOne;
+        private System.Windows.Forms.Label lblLetterTwo;
+        private System.Windows.Forms.Label lblLetterThree;
+        private System.Windows.Forms.Label lblLetterFour;
+        private System.Windows.Forms.Label lblLabelFive;
+        private System.Windows.Forms.Label lblLetterSix;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCal;
+    }
+}
+
---
+++ Gradecalculator/Gradecalculator/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gradecalculator
+{
+    public partial class averagecal : Form
+    {
+        public averagecal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            //Prompt User to enter grades
+            decimal classOne = Convert.ToDecimal(txtClassOne.Text);
+            decimal classTwo = Convert.ToDecimal(txtClassTwo.Text);
+            decimal classThree = Convert.ToDecimal(txtClassThree.Text);
+            decimal classFour = Convert.ToDecimal(txtClassFour.Text);
+            decimal classFive = Convert.ToDecimal(txtClassFive.Text);
+            decimal classSix = Convert.ToDecimal(txtClassSix.Text);
+
+
+            ///Input Grades 1 to 7
+
+
+            //Calculate (all input grades / 7 = Output with letter)
+
+            decimal finalCal = (classOne + classTwo + classThree + classFour + classFive + classSix) / 7;
+            lblCal.Text = Convert.ToString(finalCal);
+
+
+
+            //Output Class 1 Grade with Letter
+
+            if (classOne > 90 ) 
+            {
+                lblClassOne.Text = "A";
+            }
+
+           if (classTwo > 80)
+            {
+                lblClassTwo.Text = "B"; 
+            }
+        
+            if (classThree > 70)
+            {
+                lblClassThree.Text = "C";
+            }
+
+
+        }
+
+    }
+           
+            }
+
+
+
+      
+
